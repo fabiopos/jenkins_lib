c85f1a37ef43493fbf422e55b348c03aa8e29550
Fabio Posada <xFPosada@teichert.com>
2025-03-18 21:20:24 -0500

    adding sonarenv

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,6 +1,8 @@
 def call(Map config){   
-    sh "${config.SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=${config.SONAR_PROJECT_KEY} -Dsonar.sources='.' \
-    -Dsonar.host.url='http://sonarqube:9000' -Dsonar.login=${config.SONAR_TOKEN}"
+     withSonarQubeEnv('SonarQube') {
+        sh "${config.SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=${config.SONAR_PROJECT_KEY} -Dsonar.sources='.' \
+        -Dsonar.host.url='http://sonarqube:9000' -Dsonar.login=${config.SONAR_TOKEN}"
+     }
 
     timeout(time: 1, unit: 'HOURS') {
                     waitForQualityGate abortPipeline: true
