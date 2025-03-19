479e324eadf3d857550fe44296e47038942b2b83
Fabio Posada <xFPosada@teichert.com>
2025-03-18 21:06:27 -0500

    fix 4

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(){   
-    ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.sources='.' \
-    -Dsonar.host.url='http://sonarqube:9000' -Dsonar.login=${SONAR_TOKEN}
+    ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey="${SONAR_PROJECT_KEY}" -Dsonar.sources='.' \
+    -Dsonar.host.url='http://sonarqube:9000' -Dsonar.login="${SONAR_TOKEN}"
 
     timeout(time: 1, unit: 'HOURS') {
                     waitForQualityGate abortPipeline: true
