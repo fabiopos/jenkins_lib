df2395b8f4c16b771a8876b81048c52eb6c03d67
Fabio Posada <xFPosada@teichert.com>
2025-03-18 21:26:39 -0500

    abortPipeline in config

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,11 +1,11 @@
-def call(Map config, abortPipeline){   
+def call(Map config){   
      withSonarQubeEnv('SonarQube') {
         sh "${config.SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=${config.SONAR_PROJECT_KEY} -Dsonar.sources='.' \
         -Dsonar.host.url='http://sonarqube:9000' -Dsonar.login=${config.SONAR_TOKEN}"
      }
 
     timeout(time: 1, unit: 'HOURS') {
-                    waitForQualityGate abortPipeline: ${abortPipeline}
+                    waitForQualityGate abortPipeline: ${config.abortPipeline}
     }
             				
 }
